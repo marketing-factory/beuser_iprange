36d9f79099b0829ff6cbdcbd3767f3e308417b9e
Simon Schmidt <sfs@marketing-factory.de>
2015-09-04 15:24:18 +0000

    update for 7.x

--- ext_localconf.php
+++ ext_localconf.php
@@ -1,25 +1,23 @@
 <?php
-if (!defined ('TYPO3_MODE')) {
- 	die ('Access denied.');
+if (!defined('TYPO3_MODE')) {
+    die ('Access denied.');
 }
 
-t3lib_extMgm::addService($_EXTKEY,  'auth' /* sv type */,  'tx_beuseriprange_sv1' /* sv key */,
-		array(
-
-			'title' => 'IP-Range Authentication',
-			'description' => 'Authenticates against IP-range lists',
-
-			'subtype' => 'authUserBE',
-
-			'available' => TRUE,
-			'priority' => 70,
-			'quality' => 75,
-
-			'os' => '',
-			'exec' => '',
-
-			'classFile' => t3lib_extMgm::extPath($_EXTKEY).'sv1/class.tx_beuseriprange_sv1.php',
-			'className' => 'tx_beuseriprange_sv1',
-		)
-	);
-?>
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addService(
+    $_EXTKEY,
+    'auth' /* sv type */,
+    'tx_beuseriprange_sv1' /* sv key */,
+    array(
+        'title' => 'IP-Range Authentication',
+        'description' => 'Authenticates against IP-range lists',
+        'subtype' => 'authUserBE',
+        'available' => true,
+        'priority' => 70,
+        'quality' => 75,
+        'os' => '',
+        'exec' => '',
+        'classFile' => \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::extPath($_EXTKEY) .
+            'sv1/class.tx_beuseriprange_sv1.php',
+        'className' => 'tx_beuseriprange_sv1',
+    )
+);

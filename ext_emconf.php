b1b1d020b6552680ca8f52e867f6f5428033ce6f
Sebastian Fischer <typo3@evoweb.de>
2017-08-21 15:14:35 +0200

    Increase compatibility

--- ext_emconf.php
+++ ext_emconf.php
@@ -1,40 +1,17 @@
 <?php
 
-########################################################################
-# Extension Manager/Repository config file for ext "beuser_iprange".
-#
-# Auto generated 02-10-2011 07:50
-#
-# Manual updates:
-# Only the data in the array - everything else is removed by next
-# writing. "version" and "dependencies" must not be touched!
-########################################################################
-
-$EM_CONF[$_EXTKEY] = [
+$EM_CONF['beuser_iprange'] = [
     'title' => 'IP-range for Admins/Be-user',
     'description' => 'restrict BE-user to IP-range',
     'category' => 'services',
-    'shy' => 0,
     'version' => '3.0.0',
-    'dependencies' => '',
-    'conflicts' => '',
-    'priority' => '',
-    'loadOrder' => '',
-    'module' => '',
     'state' => 'stable',
-    'uploadfolder' => 0,
-    'createDirs' => '',
-    'modify_tables' => '',
     'clearcacheonload' => 1,
-    'lockType' => '',
     'author' => 'Steffen Kamper, Simon Schmidt',
     'author_email' => 'typo3@marketing-factory.de',
-    'author_company' => '',
-    'CGLcompliance' => '',
-    'CGLcompliance_note' => '',
     'constraints' => [
         'depends' => [
-            'typo3' => '6.2.0-7.9.99',
+            'typo3' => '6.2.0-8.7.99',
         ],
         'conflicts' => [],
         'suggests' => [],

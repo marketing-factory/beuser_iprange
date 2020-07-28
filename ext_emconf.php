ddd3bde2b9f665a73cfb896a45dca06c27276b73
Simon Schmidt <sfs@marketing-factory.de>
2020-07-28 15:18:00 +0200

    refactor auth service, remove support for 7LTS

--- ext_emconf.php
+++ ext_emconf.php
@@ -4,14 +4,14 @@ $EM_CONF['beuser_iprange'] = [
     'title' => 'IP-range for Admins/Be-user',
     'description' => 'restrict BE-user to IP-range',
     'category' => 'services',
-    'version' => '4.0.0',
+    'version' => '5.0.0',
     'state' => 'stable',
     'clearcacheonload' => 1,
     'author' => 'Steffen Kamper, Simon Schmidt',
     'author_email' => 'typo3@marketing-factory.de',
     'constraints' => [
         'depends' => [
-            'typo3' => '7.6.0-10.4.99',
+            'typo3' => '8.7.0-10.4.99',
         ],
         'conflicts' => [],
         'suggests' => [],
